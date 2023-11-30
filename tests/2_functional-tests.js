4c42b8d39da79bbffb7aa8a09b63befa6f504a0c
danut nanu <125583127+danutnanu@users.noreply.github.com>
2023-11-30 19:17:36 +0000

    update 2_functional-tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,12 +6,6 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 
-
-
-
-
-
-
 let validPuzzle =
     '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.'
 suite('Functional Tests', () => {
