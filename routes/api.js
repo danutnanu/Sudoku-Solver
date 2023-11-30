07ce0ec80ab9a70a27972d53c24d482c0b382c4b
danut nanu <125583127+danutnanu@users.noreply.github.com>
2023-11-30 19:15:14 +0000

    complete project

--- routes/api.js
+++ routes/api.js
@@ -9,7 +9,7 @@ module.exports = function (app) {
     const { puzzle, coordinate, value } = req.body;
     if (!puzzle || !coordinate || !value) {
       res.json({ error: 'Required field(s) missing' });
-      return;
+      return; 
     }
     if (solver.validate(puzzle) !== 'Valid') {
       res.json({ error: solver.validate(puzzle) });
@@ -20,7 +20,7 @@ module.exports = function (app) {
     if (
       coordinate.length !== 2 ||
       !/[a-i]/i.test(row) ||
-      !/[1-9]/i.test(column) 
+      !/[1-9]/i.test(column)
     ) {
       console.log('Invalid coordinate :>> ');
       res.json({ error: 'Invalid coordinate' });
@@ -30,12 +30,42 @@ module.exports = function (app) {
       res.json({ error: 'Invalid value' });
       return;
     }
-  }); 
-
-  app.route('/api/solve').post((req, res) => {
-    const puzzle = req.body.puzzle;
-    if (solver.validate(puzzle) !== 'Valid') {
-
+    let index = (solver.letterToNumber(row) - 1) * 9 + (column - 1);
+    if (puzzle[index] == value) {
+      res.json({ valid: true }); 
+      return;
+    }
+    let validCol = solver.checkColPlacement(puzzle, row, column, value);
+    let validReg = solver.checkRegionPlacement(puzzle, row, column, value);
+    let validRow = solver.checkRowPlacement(puzzle, row, column, value);
+    let conflicts = [];
+    if (validCol && validReg && validRow) {
+      res.json({ valid: true });
+    } else {
+      if (!validRow) {
+        conflicts.push('row');
+      }
+      if (!validCol) {
+        conflicts.push('column');
+      }
+      if (!validReg) {
+        conflicts.push('region');
+      }
+      res.json({ valid: false, conflict: conflicts });
     }
   });
+
+  app.route('/api/solve').post((req, res) => {
+      const puzzle = req.body.puzzle;
+      if (solver.validate(puzzle) !== 'Valid') {
+        res.json({ error: solver.validate(puzzle) });
+        return;
+      }
+      const solveString = solver.completeSudoku(puzzle);
+      if (!solveString) {
+        res.json({ error: 'Puzzle cannot be solved' });
+      } else {
+        res.json({ solution: solveString });
+      }
+    });
 };
